6d95085b87477ad4e0cf2b9b2661dc25f4aee864
Kenny Mason <kenny@khcfirm.com>
2026-06-05 14:34:34 -0400

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -146,6 +146,7 @@ SPECIALTY_GROUPS = {
     "Plastic/Reconstructive": ["plastic", "reconstructive"],
     "Psych/Behavioral": ["psychiat", "psychology", "behavioral"],
     "Extremities": ["hand", "wrist", "extremity", "extremities", "extrem", "foot", "ankle"],
+    "ENT": ["ENT"],
 }
 
 def specialty_groups_for_text(s: str):
